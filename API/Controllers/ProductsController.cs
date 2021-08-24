bbd176bd10c2c34135a0c84d8c7c97d976fd6481
Manuel Santos <manueljoaosantos@gmail.com>
2021-08-24 19:28:05 +0100

    Versão sem projectos Core e Infrastructure

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("get-all-products")]
+        public async Task<ActionResult<List<Product>>> GetAllProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+    }
+        [HttpGet("get-product-by-id/{id}")]
+        //[HttpGet("{id}")]
+        public async Task<IActionResult> GetProductById(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            return Ok(product);
+        }
+    }
+}
