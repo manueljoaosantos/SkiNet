86e9922e321918280f2c8c951ff5307d365f6b7b
Manuel Santos <manueljoaosantos@gmail.com>
2021-08-28 11:15:59 +0100

    Repositorio Genérico: Creating a specification class

---
+++ Core/Entities/BaseSpecification.cs
@@ -0,0 +1,32 @@
+﻿using Core.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class BaseSpecification<T> : ISpecification<T>
+    {
+        public BaseSpecification(Expression<Func<T, bool>> criteria, 
+            List<Expression<Func<T, object>>> includes)
+        {
+            Criteria = criteria;
+            Includes = includes;
+        }
+
+        public Expression<Func<T, bool>> Criteria { get; }
+
+        //        .Include(p => p.ProductType)
+        //        .Include(p => p.ProductBrand)
+        public List<Expression<Func<T, object>>> Includes { get; }
+        = new List<Expression<Func<T, object>>>();
+
+        protected void AddInclude(Expression<Func<T, object>> includeExpression)
+        {
+            Includes.Add(includeExpression);
+        }
+    }
+}
