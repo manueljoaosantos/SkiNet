31015ebad0f855c2bfd5c17de84cfb403d440926
Manuel Santos <manueljoaosantos@gmail.com>
2021-08-29 13:28:20 +0100

    Configuring AutoMapper profile

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -50,16 +50,17 @@ namespace API.Controllers
             var spec = new ProductsWithTypesAndBrandsSpecification();
 
             var products = await _productRepo.ListAsync(spec);
-            return products.Select(product => new ProductToReturnDto
-            {
-                Id = product.Id,
-                Name = product.Name,
-                Description = product.Description,
-                PictureUrl = product.PictureUrl,
-                Price = product.Price,
-                ProductBrand = product.ProductBrand.Name,
-                ProductType = product.ProductType.Name
-            }).ToList();
+            //return products.Select(product => new ProductToReturnDto
+            //{
+            //    Id = product.Id,
+            //    Name = product.Name,
+            //    Description = product.Description,
+            //    PictureUrl = product.PictureUrl,
+            //    Price = product.Price,
+            //    ProductBrand = product.ProductBrand.Name,
+            //    ProductType = product.ProductType.Name
+            //}).ToList();
+            return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
         }
 
         [HttpGet("get-product-by-id/{id}")]
@@ -87,7 +88,7 @@ namespace API.Controllers
             //    ProductBrand = product.ProductBrand.Name,
             //    ProductType = product.ProductType.Name
             //};
-            return _mapper.Map<Product, ProductToReturnDto>(product):
+            return _mapper.Map<Product, ProductToReturnDto>(product);
         }
 
         [HttpGet("get-all-productBrands")]
